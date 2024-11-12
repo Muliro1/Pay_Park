8393d4031a06decfaba296d5d15f818834aa4ef8
Muliro1 <mulirokhaemba@gmail.com>
2024-11-13 02:13:09 +0300

    floor model

--- parking/parking/models.py
+++ parking/parking/models.py
@@ -22,3 +22,18 @@ class Block(models.Model):
 
     def __str__(self):
         return f"Block {self.block_code} in {self.parking_lot.address}"
+
+class Floor(models.Model):
+    id = models.AutoField(primary_key=True)
+    block = models.ForeignKey(Block, on_delete=models.CASCADE, related_name='floors')
+    floor_number = models.IntegerField()
+    max_height_in_inch = models.IntegerField()
+    number_of_wings = models.IntegerField()
+    number_of_slots = models.IntegerField()
+    is_covered = models.BooleanField(default=False)
+    is_accessible = models.BooleanField(default=True)
+    is_floor_full = models.BooleanField(default=False)
+    is_reserved_reg_cust = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Floor {self.floor_number} in Block {self.block.block_code}"
