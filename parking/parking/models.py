2a9bd4430f892af21e55e399f2a8df8f3ee8e8ad
Muliro1 <mulirokhaemba@gmail.com>
2024-11-13 02:10:13 +0300

    block model

--- parking/parking/models.py
+++ parking/parking/models.py
@@ -12,3 +12,13 @@ class ParkingLot(models.Model):
 
     def __str__(self):
         return f"{self.address} - {self.operating_company}"
+
+class Block(models.Model):
+    id = models.AutoField(primary_key=True)
+    parking_lot = models.ForeignKey(ParkingLot, on_delete=models.CASCADE, related_name='blocks')
+    block_code = models.CharField(max_length=10)
+    number_of_floors = models.IntegerField()
+    is_block_full = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Block {self.block_code} in {self.parking_lot.address}"
