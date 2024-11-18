795de89cb2836d1fa561892b69fd7268d295e74b
Muliro1 <mulirokhaemba@gmail.com>
2024-11-18 16:10:07 +0300

    more

--- parking/users/views.py
+++ parking/users/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+def login_view(request):
+    # Login view implementation
+    pass
+
+def logout_view(request):
+    # Logout view implementation
+    pass
