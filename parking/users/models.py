f4747b263e024525df4931cd06d89176d9811b91
Muliro1 <mulirokhaemba@gmail.com>
2024-11-20 17:48:20 +0300

    views

--- parking/users/models.py
+++ parking/users/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
 # Create your models here.
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
 from django.db import models
 
-class User(AbstractUser):
-    # Add any additional fields you need
-    objects = models.Manager()
+class User(AbstractUser, PermissionsMixin):
+    # Add any additional fields you nee
+    groups = models.ManyToManyField(
+        'Group',related_name='user_set'
+    )
     age = models.PositiveIntegerField(null=True, blank=True)
     address = models.CharField(max_length=255, null=True, blank=True)
     phone_number = models.CharField(max_length=20, null=True, blank=True)
@@ -15,3 +17,10 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+class Group(models.Model):
+    name = models.CharField(max_length=255)
+    users = models.ManyToManyField(User, related_name='groups')
+
+    def __str__(self):
+        return self.name
--- parking/users/views.py
+++ parking/users/views.py
@@ -1,10 +1,11 @@
-
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .authentication import CustomUserBackend
+from .forms import RegistrationForm
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('login')
@@ -14,7 +15,14 @@ def register(request):
 
 def login_view(request):
     # Login view implementation
-    pass
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = CustomUserBackend().authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+    return render(request, 'login.html')
 
 def logout_view(request):
     # Logout view implementation
