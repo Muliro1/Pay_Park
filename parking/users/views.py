40df6a1fa2579dccb6566b1230e23f7bbee7d761
Muliro1 <mulirokhaemba@gmail.com>
2024-11-24 20:36:05 +0300

    user-authentication

--- parking/users/views.py
+++ parking/users/views.py
@@ -1,29 +1,52 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from .authentication import CustomUserBackend
-from .forms import RegistrationForm
+from .forms import RegisterForm
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def register(request):
     if request.method == 'POST':
-        form = RegistrationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('login')
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+    
     else:
-        form = UserCreationForm()
+        form = RegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
 def login_view(request):
+    error_message = None
     # Login view implementation
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = CustomUserBackend().authenticate(request, username=username, password=password)
+        username = request.POST.get['username']
+        password = request.POST.get['password']
+        user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('home')
-    return render(request, 'login.html')
+            next_url = request.POST.get('next') or request.GET.get('next') or 'home'  
+            return redirect('next_url')
+    return render(request, 'login.html', {'error': error_message})
 
 def logout_view(request):
-    # Logout view implementation
-    pass
+    if request.method == "POST":
+        logout(request)
+        return redirect('login')
+    else:
+        return redirect('home')
+
+@login_required
+def home_view(request):
+    return render(request, 'auth1_app/home.html')
+
+class ProtectedView(LoginRequiredMixin, View):
+    login_url = '/login/'
+    # 'next' - to redirect URL
+    redirect_field_name = 'redirect_to'
+    
+    def get(self, request):
+        return render(request, 'registration/protected.html')
